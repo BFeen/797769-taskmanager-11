9487d069c5b95ab11498d936c3053bca11acb45c
Boris <bfeen@mail.ru>
2020-04-04 16:30:23 +0300

    2.2. Added components, fix bugs using ESLint test

---
+++ src/components/board.js
@@ -0,0 +1,9 @@
+export const createBoardTemplate = () => {
+  return (
+    `<section class="board container">
+    <div class="board__filter-list">
+    </div>
+
+    <div class="board__tasks"></div>`
+  );
+};
