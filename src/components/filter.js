70393e1063a0c5f68def7363829c951659a48b65
Boris <bfeen@mail.ru>
2020-04-15 23:16:48 +0300

    4.1.1 All components changed

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,3 +1,6 @@
+import {createElement} from "../utils.js";
+
+
 const createFilterMarkup = (filter, isChecked) => {
   const {name, count} = filter;
 
@@ -15,7 +18,7 @@ const createFilterMarkup = (filter, isChecked) => {
   );
 };
 
-export const createFilterTemplate = (filters) => {
+const createFilterTemplate = (filters) => {
   const filtersMarkup = filters.map((item, index) => createFilterMarkup(item, index === 0)).join(`\n`);
   return (
     `<section class="main__filter filter container">
@@ -23,3 +26,26 @@ export const createFilterTemplate = (filters) => {
     </section>`
   );
 };
+
+export default class Filter {
+  constructor(filters) {
+    this._filters = filters;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFilterTemplate(filters)
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
