b897a2141c60f4dd52047bc2ff3b603e4a930916
Boris <bfeen@mail.ru>
2020-04-20 16:02:16 +0300

    5.1 Added new AbstractComponent and extensions for all classes

--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,6 @@
 import {MONTH_NAMES} from "../const.js";
-import {formatTime, createElement} from "../utils.js";
+import {formatTime} from "../utils.js";
+import AbstractComponent from "./abstract-component.js";
 
 const createTaskTemplate = (task) => {
   const {description, dueDate, color, repeatingDays, isArchive, isFavorite} = task;
@@ -62,25 +63,13 @@ const createTaskTemplate = (task) => {
   );
 };
 
-export default class Task {
+export default class Task extends AbstractComponent {
   constructor(task) {
-    this._task = task;
-    this._element = null;
-  }
+    super();
 
+    this._task = task;
+  }
   getTemplate() {
     return createTaskTemplate(this._task);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
