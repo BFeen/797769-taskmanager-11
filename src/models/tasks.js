67b5201c8943480a98aef0a6bdcacfcdf51e5c19
Boris <bfeen@mail.ru>
2020-05-13 12:38:42 +0300

    7.1.3 Set-up controllers for filtering

--- src/models/tasks.js
+++ src/models/tasks.js
@@ -1,37 +1,56 @@
+import {getTasksByFilter} from "../utils/filter.js";
+import {FilterType} from "../const.js";
+
+
 export default class TasksModel {
-    constructor() {
-        this._tasks = [];
+  constructor() {
+    this._tasks = [];
+    this._activeFilterType = FilterType.ALL;
 
-        this._dataChangeHandlers = [];
+    this._dataChangeHandlers = [];
+    this._filterChangeHandlers = [];
+  }
+
+  getTasks() {
+    return getTasksByFilter(this._tasks, this._activeFilterType);
+  }
+
+  getTasksAll() {
+    return this._tasks;
+  }
+
+  setTasks(tasks) {
+    this._tasks = Array.from(tasks);
+    this._callHandlers(this._dataChangeHandlers);
+  }
+
+  setFilter(filterType) {
+    this._activeFilterType = filterType;
+    this._callHandlers(this._filterChangeHandlers);
+  }
+
+  updateTask(id, task) {
+    const index = this._tasks.findIndex((it) => it.id === id);
+
+    if (index === -1) {
+      return false;
     }
 
-    getTasks() {
-        return this._tasks;
-    }
+    this._tasks = concat(this._tasks.slice(0, index), task, this._tasks.slice(index + 1));
+    this._callHandlers(this._dataChangeHandlers);
+    
+    return true;
+  }
 
-    setTasks(tasks) {
-        this._tasks = Array.from(tasks);
-        this._callHandlers(this._dataChangeHandlers);
-    }
+  setFilterChangeHandler(handler) {
+    this._filterChangeHandlers.push(handler);
+  }
 
-    updateTask(id, task) {
-        const index = this._tasks.findIndex((it) => it.id === id);
+  setDataChangeHandler(handler) {
+    this._dataChangeHandlers.push(handler);
+  }
 
-        if (index === -1) {
-            return false;
-        }
-
-        this._tasks = concat(this._tasks.slice(0, index), task, this._tasks.slice(index + 1));
-        this._callHandlers(this._dataChangeHandlers);
-        
-        return true;
-    }
-
-    setDataChangeHandler(handler) {
-        this._dataChangeHandlers.push(handler);
-    }
-
-    _callHandlers(handlers) {
-        handlers.forEach((handler) => handler());
-    }
+  _callHandlers(handlers) {
+    handlers.forEach((handler) => handler());
+  }
 }
